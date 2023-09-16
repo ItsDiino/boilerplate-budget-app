04bc63398fbb99fbb759da850594ebf2662242fe
Diino <gameroni13@gmail.com>
2023-09-16 09:30:59 +0200

    did some stuff

--- budget.py
+++ budget.py
@@ -1,7 +1,26 @@
 class Category:
 
+    def __init__(self, category):
+        self.name = category
+        self.ledger = []
 
+    def deposit(self, amount, description=''):
+        deposit = {'amount': amount, "description": description}
+        self.ledger.append(deposit)
 
+    def withdraw(self, amount, description=''):
+        if self.check_funds(-amount):
+            withdraw = {'amount': -amount, "description": description}
+            self.ledger.append(withdraw)
+            return True
+        else:
+            return False
+        
+    def get_balance(self):
+        self.ledger[0]['amount'] += amount
+        print(self.ledger)
+        return self.ledger[0]['amount']
 
-
-def create_spend_chart(categories):
+    def check_funds(self, amount):
+        amount >= self.get_balance(amount)
+        return True
